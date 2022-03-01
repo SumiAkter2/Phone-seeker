82079523518ad730c84eb0612dbf1ee0f35235e7
SumiAkter2 <akteralisha36@gmail.com>
2022-03-01 16:46:52 +0600

    added somthing

--- js/script.js
+++ js/script.js
@@ -1,25 +1,47 @@
 console.log('allah');
-// Onclick button setup and search input value show:
+
+// toggle spinner:
+
+const toggleSpinner= displayStyle => {
+  document.getElementById('spinner').style.display=displayStyle;
+}
+
+//  const toggleSearchResult= displayStyle => {
+//    document.getElementById('search-result').style.display=displayStyle;
+//  }
+ //Onclick button setup and search input value show:
 const loadPhone=()=>{
 const searchInput = document.getElementById('search-id');
+//display spinner:
+toggleSpinner('block');
+//  toggleSearchResult('none');
+
+
 const searchText = searchInput.value ;
 searchInput.value = ' ';
  
-// fetch url for search result:
+
+ // fetch url for search result:
     const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
      fetch(url)
     .then(res=>res.json())
     .then(data=>displayResult(data.data.slice(0,20)));
+   
     }
+
+
+   ///////////////////////////////////////////////////// 
 // display result:
 const displayResult =phones=>{
-  console.log(phones)
+ 
     const searchResult = document.getElementById('search-result');
       searchResult.textContent='';
       
    phones?.forEach(phone=>{
          console.log(phone);
         const div= document.createElement('div');
+
+        
         div.classList.add('col');
         div.innerHTML= `
         <div class= "card p-4  h-100">
@@ -34,6 +56,8 @@ const displayResult =phones=>{
         `;
         searchResult.appendChild(div);
     }); 
+    toggleSpinner('none')
+     
 }
 
 //
@@ -54,21 +78,21 @@ const displayResult =phones=>{
  div.innerHTML=`
         <div class="row g-0 ps-4">
          <div class="col-md-4 ">
-              <img src="${phone.image}" class="mt-3 img-fluid card-img-top" alt="...">
+              <img src="${phone.image}" class="img-margin img-fluid card-img-top" alt="...">
         </div>
      <div class="col-md-8">
       <div class="card-body">
            <h3>${phone.name}</h3>
-           <p class="fs-6 card-text">Release Date: ${phone.releaseDate}</p>
-           <p class="card-text">Id: ${phone.slug}</p>
+            <p class="fs-6 card-text">Release Date: ${phone.releaseDate ? phone.releaseDate: 'Not available'}</p>
+            <p >Id: ${phone.slug}</p>
             <h4>Main-freature:</h4>
-            <p class="card-text">
-               Storage: ${phone.mainFeatures.storage}</p>
-            <p class="card-text">Display-size: ${phone.mainFeatures.displaySize}</p>
-            <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}</p>
-           <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
-           <p class="card-text">Sensor: ${phone.mainFeatures.sensors}</p>
-          
+            <p > Storage: ${phone.mainFeatures.storage}</p>
+            <p >Display-size: ${phone.mainFeatures.displaySize}  </p>
+            <p >Chipset: ${phone.mainFeatures.chipSet}</p>
+           <p >Memory: ${phone.mainFeatures.memory}</p>
+           <p class="card-text ">Sensor: ${phone.mainFeatures.sensors}</p>
+           <h4>Others:</h4>
+          <p> bluetooth: ${phone.others ? phone.others.Bluetooth: 'no'}, Radio:${phone.others ? phone.others.Radio: 'Not available'}</p>
       </div>
     </div>
   </div>
