130ea54a30d75f22b26f55478bb767840c688880
SumiAkter2 <akteralisha36@gmail.com>
2022-03-01 20:01:00 +0600

    added rounded

--- js/script.js
+++ js/script.js
@@ -35,7 +35,9 @@ fetch(url)
 .then(res => res.json())
 .then(data => displayResult(data.data.slice(0,20)))
 
+
 }
+
 
 
    ///////////////////////////////////////////////////// 
@@ -54,7 +56,7 @@ const displayResult =phones=>{
         const div= document.createElement('div');
       
         const errorMessage = document.getElementById('error-message').style.display='none';
-
+       
         div.classList.add('col');
         div.innerHTML= `
         <div class= "card p-4  h-100">
@@ -66,8 +68,10 @@ const displayResult =phones=>{
             </div>
          
         </div>
+
         `;
         searchResult.appendChild(div);
+       
     }); 
     toggleSpinner('none');
     toggleDetailResult('none');
@@ -89,23 +93,23 @@ const displayResult =phones=>{
    searchResult.textContent='';
  const div = document.createElement('div');
  div.innerHTML=`
-        <div class="row g-0 ps-4">
+        <div class="row g-0 ps-4 rounded">
          <div class="col-md-4 ">
-              <img src="${phone.image}" class="img-margin img-fluid card-img-top" alt="...">
+              <img  src="${phone.image}" class=" img-margin img-fluid card-img-top" alt="...">
         </div>
      <div class="col-md-8">
       <div class="card-body">
-           <h3>${phone.name}</h3>
+            <h3>${phone.name}</h3>
             <p class="fs-6 card-text"><span class="fw-bold">Release Date: </span> ${phone.releaseDate ? phone.releaseDate: 'Not available'}</p>
             <p ><span class="fw-bold">Id: </span> ${phone.slug}</p>
-            <h4>Main-freature:</h4>
+            <p class=" fw-bold">Main-freature:</p>
             <p > <span class="fw-bold">Storage:</span> ${phone.mainFeatures.storage}</p>
             <p ><span class="fw-bold">Display-size:</span> ${phone.mainFeatures.displaySize}  </p>
             <p > <span class="fw-bold">Chipset:</span> ${phone.mainFeatures.chipSet}</p>
            <p ><span class="fw-bold">Memory: </span> ${phone.mainFeatures.memory}</p>
-           <p class="card-text "> <span class="fw-bold">Sensor:</span> ${phone.mainFeatures.sensors}</p>
-           <h4>Others:</h4>
-          <p> <span class="fw-bold">Bluetooth: </span>${phone.others ? phone.others.Bluetooth: 'Not available'}, <span class="fw-bold">Radio:</span>${phone.others ? phone.others.Radio: 'Not available'}</p>
+           <p class="card-text fs-6"> <span class="fw-bold ">Sensor:</span> ${phone.mainFeatures.sensors}</p>
+           <p class="fw-bold fs-4">Others:</p>
+          <p> <span class="fw-bold">Bluetooth: </span>${phone.others ? phone.others.Bluetooth: 'Not available'}, <span class="fw-bold fs-6">Radio:</span>${phone.others ? phone.others.Radio: 'Not available'} ,<span class="fw-bold"> WLAn: </span>${phone.others ? phone.others.WLAN: 'Not available'},<span class="fw-bold"> GPS: </span>${phone.others ? phone.others.GPS: 'Not available'}, <span class="fw-bold">NFC: </span> ${phone.others ? phone.others.NFC: 'Not available'}</p>
       </div>
     </div>
   </div>
