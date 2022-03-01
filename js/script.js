f1cc45dfb7fa8fb85a2952ccf4b4a06106338acd
SumiAkter2 <akteralisha36@gmail.com>
2022-03-01 09:42:59 +0600

    added input value and fetch url

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('allah')
+console.log('allah');
+
+const loadPhone=()=>{
+const searchInput = document.getElementById('search-id');
+const searchText = searchInput.value ;
+searchInput.value = ' ';
+console.log(searchText);
+
+const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+fetch(url)
+.then(res=>res.json())
+.then(data=>console.log(data.data));
+
+}
+
